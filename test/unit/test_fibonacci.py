6db1978f9281bffa6c9ad81c8f7345e4ea4aff5a
Borisov Konstantin <borisov.ks@gmail.com>
2018-11-06 12:41:56 +0300

    #13 some rewrite tests

--- test/unit/test_fibonacci.py
+++ test/unit/test_fibonacci.py
@@ -1,10 +1,16 @@
 import pytest
+from django.http import JsonResponse
+from django.test import RequestFactory
 
 from fibonacci.apps.core.views import FiboView
 
 
 @pytest.mark.parametrize('param', [(0, 144), (1, 144), (5, 46368), (5, 144), (0, 56554)])
 def test_short(param):
-    resp = FiboView.fibo_generator(_from=param[0], _to=param[1])
-    assert isinstance(resp, list)
-    assert True
+    request_factory = RequestFactory()
+    request = request_factory.get('/fibonacci/', data=dict(_from=param[0], _to=param[1]))
+    view = FiboView.as_view()
+    response = view(request)
+    assert response.status_code == 200
+    assert isinstance(response, JsonResponse)
+    assert 'result' in response.content.decode()
